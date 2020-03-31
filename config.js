13eadc42819a5dca3005cc6d3e711162a77e02df
paulobusch <paulo202015@outlook.com.br>
2020-03-30 23:53:00 -0300

    Correções

--- config.js
+++ config.js
@@ -1,12 +1,21 @@
 module.exports = {
     Config: {
-        idUser: 'oiyhjkmn',
         total: 187845603,
-        limit: 1000000,
+        limit: 10000,
     },
     FdbConfig: {
         database: '/firebird/databases/customers_database.fdb',
         user: 'sysdba',
         password: 'masterkey'
     },
+    MySqlConfig: {
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '123456',
+        database: 'vetorial'
+    },
+    Columns: {
+        peoples: ['id', 'full_name', 'last_name', 'cpf', 'cnpj', 'address', 'complement', 'neighborhood', 'cep', 'city_name', 'state_uf', 'birth_date', 'phone']
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-const { Config, FdbConfig } = require('../config');
+const { Config, Columns, FdbConfig, MySqlConfig } = require('../config');
+const { Query } = require('./utils/query');
 
 var Firebird = require('node-firebird');
-const _ = require('lodash');
+const mysql = require('sync-mysql');
 
-console.log('Atualizando...');
+const connection = new mysql(MySqlConfig);
+const getText = (txt) => {
+    if (!txt) return null;
+    return txt
+        .replace(/[^a-z^ ]/gi, '');
+}
+const getNumber = (txt) => {
+    if (!txt) return null;
+    return txt
+        .replace(/[^0-9]/gi, '');
+}
+const castStr = (raw) => raw ? raw.toString('utf-8') : raw;
+const castDate = (raw) => raw ? new Date(castStr(raw)) : raw;
 
+console.log('importando...');
 const recursive = (offset, time) => {
     if (offset >= Config.total) {
         console.log('Fim...');
@@ -16,10 +30,48 @@ const recursive = (offset, time) => {
     const startTime = new Date();
     pool.get((err, db) => {
         if (err) throw err;
-        const query = `UPDATE CONSULTA1 SET CON_SOBRENOME=TRIM(RIGHT(CON_NOME, POSITION(' ', REVERSE(CON_NOME)))) where CON_CODIGO >= ${offset} and CON_CODIGO < ${offset + Config.limit};`;
-        db.query(query, null, (err, result) => {
+        const query = `
+            select 
+                first ${Config.limit} skip ${offset}
+                CON_CODIGO as ID,
+                CON_CPFCNPJ as CPF_CNPJ,
+                CON_NOME as FULL_NAME,
+                CON_SOBRENOME as LAST_NAME,
+                CON_ENDERECO as ADDRESS,
+                CON_COMPLEMENTO as COMPLEMENT,
+                CON_BAIRRO as NEIGHBORHOOD,
+                CON_CEP as CEP,
+                CON_CIDADE as CITY_NAME,
+                CON_UF as STATE_UF,
+                CON_DATANASCIMENTO as BIRTH_DATE,
+                CON_FONE as PHONE
+            from consulta;`;
+        db.query(query, null, async (err, list) => {
             if (err) throw err;
             db.detach();
+
+            const ImportRows = [];
+            for (let index in list) {
+                const raw = list[index];
+                const cpf_cnpj = getNumber(castStr(raw['CPF_CNPJ'])) || '';
+                ImportRows.push([
+                    raw['ID'],
+                    getText(castStr(raw['FULL_NAME'])),
+                    getText(castStr(raw['LAST_NAME'])),
+                    cpf_cnpj.length === 11 ? cpf_cnpj : null,
+                    cpf_cnpj.length === 14 ? cpf_cnpj : null,
+                    castStr(raw['ADDRESS']),
+                    castStr(raw['COMPLEMENT']),
+                    castStr(raw['NEIGHBORHOOD']),
+                    castStr(raw['CEP']),
+                    getText(castStr(raw['CITY_NAME'])),
+                    getText(castStr(raw['STATE_UF'])),
+                    castDate(raw['BIRTH_DATE']),
+                    castStr(raw['PHONE'])
+                ]);
+            }
+
+            await connection.query(Query.get('peoples', Columns.peoples, ImportRows));
             recursive(offset + Config.limit, startTime);
         });
     });
